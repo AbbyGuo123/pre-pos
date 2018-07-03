48c764058ebb1b47926d00e943dc462dad1d4e31
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-03 22:30:52 +0800

    [GUOAB] TEST3

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -9,7 +9,21 @@ function countSameElements(collection) {
   array3 = collection[0].split(' ');
   array.push(array3[0]);
   for(var i=0;i<collection.length;i++){
-    array3 = collection[i].split(' ');
+    if(collection[i].indexOf('[')!=-1){
+      array3 = collection[i].split('[');
+      collection[i] = array3[0]+'-'+array3[1].substring(0, array3.length - 1);
+      // collection[i]=collection[i].substring(0,collection[i].length-2);
+
+    }
+    else if(collection[i].indexOf(':')!=-1){
+      array3 = collection[i].split(':');
+      collection[i] = array3[0]+'-'+array3[1];
+    }
+    else if(collection[i].indexOf('-')!=-1){
+      array3 = collection[i].split('-');
+      collection[i] = array3[0]+'-'+array3[1];
+    }
+    array3 = collection[i].split('-');
     for(var j=0;j<array.length;j++) {
       if (array[j]==array3[0]) {
         flag=1;
@@ -23,18 +37,20 @@ function countSameElements(collection) {
   for(var i=0;i<array.length;i++) {
     array4.push(0);
   }
-
+  console.log('kkkk'+collection);
   console.log("array" + ', ' + array);
-  console.log(4 + ", " + array4);
+  console.log(4 + ", " + collection);
 
   for (var i = 0; i < collection.length; i++) {
+    array3 = collection[i].split('-');
     for(var j=0;j<array.length;j++) {
-      console.log(array3.length)
       if (array[j] == array3[0] && array3.length==1){
         array4[j]++;
       }
       else if(array[j] == array3[0]&& array3.length!=1){
-        f=parseInt(array3[2]);
+        var l = array3.substring(1,array3.length-1);
+        console.log("AAAA"+l);
+        f=parseInt(l);
         array4[j]+=f;
       }
     }
